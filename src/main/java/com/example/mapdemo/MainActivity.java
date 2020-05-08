720bc0fd36b3e23b78a0695635e32e5c54817afb
mossi1965 <simin@sbb.rs>
2020-05-08 19:59:33 +0200

    GoogleMapDemo

--- src/main/java/com/example/mapdemo/MainActivity.java
+++ src/main/java/com/example/mapdemo/MainActivity.java
@@ -38,7 +38,7 @@ public final class MainActivity extends AppCompatActivity
         implements AdapterView.OnItemClickListener {
 
 
-    /**
+    /** git
      * A custom array adapter that shows a {@link FeatureView} containing details about the demo.
      */
     private static class CustomArrayAdapter extends ArrayAdapter<DemoDetails> {
